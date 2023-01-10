55688cccb2aaadc5d044b064dd250f64963948d5
IvanVojnic <ivojnic44@gmail.com>
2023-01-10 19:53:42 +0300

    init: site

---
+++ ind.js
@@ -0,0 +1,26 @@
+//window.onscroll = function() {myFunction()};
+
+//let header = document.getElementById("");
+
+function myFunction() {
+   /* if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }*/
+}
+
+window.onload = (event) => {
+    let circle1 = document.getElementById("circle1")
+    let circle2 = document.getElementById("circle2")
+    let line1 = document.getElementById("vLine1")
+
+    let circle1Coords = circle1.getBoundingClientRect()
+    let circle2Coords = circle2.getBoundingClientRect()
+    let line1Coords = line1.getBoundingClientRect()
+
+    //line1.style.height =
+};
+
+
+
