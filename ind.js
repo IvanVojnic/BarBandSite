efc47ba91b1df9954a7d77a11d90f4b562ff1566
IvanVojnic <ivojnic44@gmail.com>
2023-01-11 04:06:37 +0300

    feat: section 3

--- ind.js
+++ ind.js
@@ -1,26 +1,26 @@
-//window.onscroll = function() {myFunction()};
-
-//let header = document.getElementById("");
-
-function myFunction() {
-   /* if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
-    }*/
-}
+let circle1 = document.getElementById("circle1")
+let circle2 = document.getElementById("circle2")
+let section1 = document.getElementById("section1")
+let line1 = document.getElementById("vLine1")
 
 window.onload = (event) => {
-    let circle1 = document.getElementById("circle1")
-    let circle2 = document.getElementById("circle2")
-    let line1 = document.getElementById("vLine1")
+    reCountLineCoords(line1, circle1, circle2, section1)
+};
 
+window.onresize = (event) => reCountLineCoords(line1, circle1, circle2, section1)
+
+function reCountLineCoords(line, circle1, circle2, section){
     let circle1Coords = circle1.getBoundingClientRect()
     let circle2Coords = circle2.getBoundingClientRect()
-    let line1Coords = line1.getBoundingClientRect()
+    let line1Coords = line.getBoundingClientRect()
+    let section1Coords = section.getBoundingClientRect()
 
-    //line1.style.height =
-};
+    line.style.left = (Number(circle1Coords.left) + Number(circle2Coords.width/2) - 2) + "px"
+    line.style.top = (Number(circle1Coords.top) - Number(section1Coords.top) + Number(circle1Coords.height)) + "px"
+    line.style.height = (Number(circle2Coords.top) - Number(circle1Coords.bottom)) + "px"
+}
+
+
 
 
 
